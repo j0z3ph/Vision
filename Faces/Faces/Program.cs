c6e4e68219985da134de3c6a2ca73603f9649d0f
Jose Luis Cruz <j0z3ph@gmail.com>
2022-11-23 00:29:34 -0600

    Added Face detector example

---
+++ Faces/Faces/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenCvSharp;
+
+namespace Faces {
+    internal class Program {
+        static void Main(string[] args) {
+            using Window window = new Window("Video");
+            using VideoCapture video = new VideoCapture(0);
+            using Mat image = new Mat();
+            using CascadeClassifier haarCascade = new CascadeClassifier(@"C:\src\progavan\Vision\haarcascades\haarcascade_frontalface_alt2.xml");
+
+            while (Cv2.WaitKey(30) != 27) {
+                video.Read(image);
+                using Mat gray = image.CvtColor(ColorConversionCodes.BGR2GRAY);
+                Rect[] faces = haarCascade.DetectMultiScale(gray);
+                foreach (var face in faces) {
+                    image.Rectangle(face, new Scalar(0,255,0));
+                }
+                window.ShowImage(image);
+            }
+
+        }
+    }
+}
