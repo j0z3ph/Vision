3983b180a2f5558dddc4975f505cc04c0458301f
Jose Luis Cruz <j0z3ph@gmail.com>
2024-06-03 13:49:45 -0600

    Minor test

--- Python/Vision/Filtros/sobel.py
+++ Python/Vision/Filtros/sobel.py
@@ -1,19 +1,25 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("lenna.png", cv2.IMREAD_GRAYSCALE)
 
-sobel_x = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
-sobel_y = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
+vid = cv2.VideoCapture(0)
 
-gx = cv2.filter2D(img, -1, sobel_x)
-gy = cv2.filter2D(img, -1, sobel_y)
+while cv2.waitKey(1) != 27:
+    ret, img = vid.read()
 
-g =  np.abs(gx) + np.abs(gy)
+    #img = cv2.imread("lenna.png", cv2.IMREAD_GRAYSCALE)
 
-cv2.imshow("Original", img)
-cv2.imshow("X", gx)
-cv2.imshow("Y", gy)
-cv2.imshow("G", g)
+    sobel_x = np.array([[-1,0,1],[-2,0,2],[-1,0,1]])
+    sobel_y = np.array([[-1,-2,-1],[0,0,0],[1,2,1]])
 
-cv2.waitKey()
+    gx = cv2.filter2D(img, -1, sobel_x)
+    gy = cv2.filter2D(img, -1, sobel_y)
+
+    g =  np.abs(gx) + np.abs(gy)
+
+    cv2.imshow("Original", img)
+    cv2.imshow("X", gx)
+    cv2.imshow("Y", gy)
+    cv2.imshow("G", g)
+
+cv2.destroyAllWindows()
