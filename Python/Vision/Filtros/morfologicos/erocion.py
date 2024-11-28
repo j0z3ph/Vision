4f640fa29b042b79e491774550f63043191c6b91
Jose Luis Cruz <j0z3ph@gmail.com>
2024-11-27 18:40:04 -0600

    Operadores morfologicos

---
+++ Python/Vision/Filtros/morfologicos/erocion.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("test.png")
+
+#cv2.MORPH_CROSS
+#cv2.MORPH_RECT
+#cv2.MORPH_ELLIPSE
+
+kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (5,5))
+#print(kernel)
+
+res = cv2.erode(img, kernel)
+
+cv2.imshow("test", img)
+cv2.imshow("Erocion", res)
+cv2.waitKey()
